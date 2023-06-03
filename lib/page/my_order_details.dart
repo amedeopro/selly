27f934eec8306f159b973004250bcec999fc37bd
Amedeo <amedeopro@me.com>
2023-06-03 13:02:15 +0200

    fix ritorno alla lista ordini da dettaglio ordine

--- lib/page/my_order_details.dart
+++ lib/page/my_order_details.dart
@@ -5,78 +5,83 @@ import 'package:selly/model/order_model.dart';
 import 'package:selly/model/product_model.dart';
 
 class MyOrderDetails extends StatefulWidget {
-
   OrderModel? order = OrderModel(
       id: 0,
       shipment: '',
       created_at: '',
       total: '',
-      products:  <ProductModel>[
+      products: <ProductModel>[
         ProductModel(
-        imageUrl: '',
-        name: '',
-        description: '',
-        price: 0,
-        fidelityPoint: 0,
-        categoryId: [0]
-    )
-  ]);
+            imageUrl: '',
+            name: '',
+            description: '',
+            price: 0,
+            fidelityPoint: 0,
+            categoryId: [0])
+      ]);
 
-      MyOrderDetails({this.order});
+  MyOrderDetails({this.order});
 
   @override
   State<MyOrderDetails> createState() => _MyOrderDetailsState();
 }
 
 class _MyOrderDetailsState extends State<MyOrderDetails> {
-
   final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: appBar(title: 'Ordine n. ${widget.order!.id}', subtitle: "del ${widget.order!.created_at}", iconBack: true, scaffoldKey: null, scf_context: context),
+        appBar: appBar(
+            title: 'Ordine n. ${widget.order!.id}',
+            subtitle: "del ${widget.order!.created_at}",
+            iconBack: true,
+            scaffoldKey: null,
+            scf_context: context,
+            return_to_home: false
+        ),
         drawer: drawer(context),
         body: Column(
           children: [
             Expanded(
               child: ListView.builder(
                 itemCount: widget.order!.products.length,
-                itemBuilder: (context, index){
+                itemBuilder: (context, index) {
                   //TODO: mancano le quantitá dei prodotti acquistati PD
                   return ListTile(
-                    title: Text(widget.order!.products[index].name) ,
-                    trailing: Text(widget.order!.products[index].quantity.toString()),
-                    subtitle: Text('€ ${widget.order!.products[index].total} cad.'),
+                    title: Text(widget.order!.products[index].name),
+                    trailing:
+                        Text(widget.order!.products[index].quantity.toString()),
+                    subtitle:
+                        Text('€ ${widget.order!.products[index].total} cad.'),
                   );
                 },
               ),
             ),
             Container(
-            margin: EdgeInsets.only(bottom: 10),
-                child: Text('Totale ordine (spese di spedizione incluse): ', style: TextStyle(
-                  fontWeight: FontWeight.w400
-                ),),
+              margin: EdgeInsets.only(bottom: 10),
+              child: Text(
+                'Totale ordine (spese di spedizione incluse): ',
+                style: TextStyle(fontWeight: FontWeight.w400),
+              ),
             ),
             Container(
               margin: EdgeInsets.only(bottom: 20),
-              child: Text('€ ${widget.order!.total}', style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20
-              ),),
+              child: Text(
+                '€ ${widget.order!.total}',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
             ),
             Container(
               width: double.infinity,
               height: 50,
               child: ElevatedButton(
-                onPressed: () {
-                },
+                onPressed: () {},
                 style: ElevatedButton.styleFrom(
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20)
-                    ),
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20)),
                   ),
                   backgroundColor: Colors.green,
                 ),
@@ -84,7 +89,6 @@ class _MyOrderDetailsState extends State<MyOrderDetails> {
               ),
             ),
           ],
-        )
-    );
+        ));
   }
 }
